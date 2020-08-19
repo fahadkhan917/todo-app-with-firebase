b23c1c284a4fcbcf6c1ad852c22c2d0bc2e8e912
fahadkhan917 <fk16181@gmail.com>
2020-08-20 01:54:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,30 +2,34 @@
 var list=document.getElementById("list");
 function edit(){
 var val=document.getElementById("val");
-var l=document.createElement('li');
-var text=document.createTextNode(val.value);
-l.appendChild(text);
-list.append(l);
+var database=firebase.database().ref('todos');
+var key=database.push().key;
+console.log(key)
 
-val.value=" ";
+// var l=document.createElement('li');
+// var text=document.createTextNode(val.value);
+// l.appendChild(text);
+// list.append(l);
 
-
-//deletebtn
-var deletebtn=document.createElement('button');
-var delbtntxt=document.createTextNode('Delete');
-
-
-deletebtn.appendChild(delbtntxt);
-deletebtn.setAttribute("onclick","dele(this)");
-l.appendChild(deletebtn);
+// val.value=" ";
 
 
-//editbtn
-var editbtn=document.createElement('button');
-var editbtntxt=document.createTextNode("Edit");
-editbtn.appendChild(editbtntxt);
-editbtn.setAttribute("onclick","ed(this)");
-l.appendChild(editbtn);
+// //deletebtn
+// var deletebtn=document.createElement('button');
+// var delbtntxt=document.createTextNode('Delete');
+
+
+// deletebtn.appendChild(delbtntxt);
+// deletebtn.setAttribute("onclick","dele(this)");
+// l.appendChild(deletebtn);
+
+
+// //editbtn
+// var editbtn=document.createElement('button');
+// var editbtntxt=document.createTextNode("Edit");
+// editbtn.appendChild(editbtntxt);
+// editbtn.setAttribute("onclick","ed(this)");
+// l.appendChild(editbtn);
 
 
 
@@ -33,29 +37,29 @@ l.appendChild(editbtn);
 
 
 }
-console.log(firebase);
+// console.log(firebase);
 
-function del(){
+// function del(){
  
-list.innerHTML='';
+// list.innerHTML='';
 
 
-}
+// }
 
-function dele(e){
+// function dele(e){
 
-console.log(e.parentNode)
-e.parentNode.remove();
-}
-
-
-function ed(e){
+// console.log(e.parentNode)
+// e.parentNode.remove();
+// }
 
 
-var val=e.parentNode.firstChild.nodeValue;
-var edi=prompt("enter the change",val);
-e.parentNode.firstChild.nodeValue=edi;
+// function ed(e){
+
+
+// var val=e.parentNode.firstChild.nodeValue;
+// var edi=prompt("enter the change",val);
+// e.parentNode.firstChild.nodeValue=edi;
 
 
 
-}
+// }
